2fe46e69864e9317e5382cdfc5a31bdf45ea28ec
Yaroslav Vishnevsky <yaroslav@4ib.com>
2017-04-07 14:02:19 +0300

    commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load("1000049.xml");  
+
+            XmlNodeList xnList = xml.SelectNodes("/ZVIT/ORG/CARD/DOCUMENT/*[@TAB=0 and @LINE=0 and @NAME='FIRM_NAME']");
+            foreach (XmlNode xn in xnList)
+            {
+                Console.WriteLine(xn.InnerText);
+            }
+            Console.ReadKey();
+        }
+    }
+}
